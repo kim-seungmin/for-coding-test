272d60ac6b44b247d7094b6cfd175cf93faa9ca8
kim-seungmin <54826101+kim-seungmin@users.noreply.github.com>
2022-10-07 16:23:25 +0900

    Create 8.3 1로 만들기.java

---
+++ 8 다이나믹 프로그래밍/8.3 1로 만들기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{    	
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		int[] arr = new int[x+1];
+		for(int i=2;i<=x;i++) {
+			arr[i]=arr[i-1]+1;
+			if(i%2==0)arr[i]=Math.min(arr[i], arr[i/2]+1);
+			if(i%3==0)arr[i]=Math.min(arr[i], arr[i/3]+1);
+			if(i%5==0)arr[i]=Math.min(arr[i], arr[i/5]+1);
+		}
+		
+		System.out.println(arr[x]);
+	}	
+}
